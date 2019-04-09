9c2551f9ee8730ad2d16aabce95e56cfd55b4ce5
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-09 16:20:46 +0200

    test suite 2

---
+++ src/main/java/TestSuite_2.java
@@ -0,0 +1,36 @@
+import org.junit.*;
+
+public class TestSuite_2 {
+
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("This is before class method");
+    }
+
+    @Before
+    public void before(){
+        System.out.println("This is before method");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("This is test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("This is test 2");
+    }
+
+    @After
+    public void after(){
+        System.out.println("This is after method");
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("This is after class method");
+    }
+
+}
