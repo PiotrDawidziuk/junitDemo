125ca3a1d02b57783a80cc03329a3fd95ef0a22b
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-09 16:09:10 +0200

    before and after methods

--- src/main/java/TestSuite_1.java
+++ src/main/java/TestSuite_1.java
@@ -1,10 +1,22 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSuite_1 {
 
+    @Before
+    public void before(){
+        System.out.println("This is before method");
+    }
+
     @Test
     public void test1(){
         System.out.println("This is test 1");
+    }
+
+    @After
+    public void after(){
+        System.out.println("This is after method");
     }
 
 }
