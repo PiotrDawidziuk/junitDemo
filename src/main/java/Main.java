02ab15670f1b57d0eb782073a6da28d907c291ba
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-09 23:02:38 +0200

    retry class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        JUnitCore.runClasses(TestSuite_1.class);
+        JUnitCore.runClasses(TestPrimer.class);
 
     }
 }
--- src/main/java/TestPrimer.java
+++ src/main/java/TestPrimer.java
@@ -2,6 +2,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestSuite_1.class,TestSuite_2.class})
+@Suite.SuiteClasses({
+        TestSuite_1.class,
+        TestSuite_2.class
+})
 public class TestPrimer {
 }
