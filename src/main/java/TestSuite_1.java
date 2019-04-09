b1a2830cd35f3a9c28d32f6234201309ec1d3f8e
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-09 16:10:24 +0200

    test2

--- src/main/java/TestSuite_1.java
+++ src/main/java/TestSuite_1.java
@@ -14,6 +14,11 @@ public class TestSuite_1 {
         System.out.println("This is test 1");
     }
 
+    @Test
+    public void test2(){
+        System.out.println("This is test 2");
+    }
+
     @After
     public void after(){
         System.out.println("This is after method");
